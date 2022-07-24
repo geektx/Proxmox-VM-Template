a9a395a217d0e4c69bd8868083ae3004e6466dad
geektx <geektx@gmail.com>
2022-07-24 18:48:44 +0000

    more typos

--- build-image.sh
+++ build-image.sh
@@ -19,20 +19,20 @@ wget ${cloud_img_url}
 
 # insert commands to populate the currently empty build-info file
 touch ${install_dir}build-info
-echo "Base Image: "$(image_name) > ${install_dir}build-info
+echo "Base Image: "${image_name} > ${install_dir}build-info
 echo "Packages added at build time: "${package_list} >> ${install_dir}build-info
 echo "Build date: "$(date) >> ${install_dir}build-info
-echo "Build creator: "$(creator) >> ${install_dir}build-info
+echo "Build creator: "${creator} >> ${install_dir}build-info
 
 virt-customize --update -a ${image_name}
 virt-customize --install ${package_list} -a ${image_name}
-virt-customize --mkdir $(build-info-file-location) --copy-in ${install_dir}build-info:$(build-info-file-location) -a ${image_name}
+virt-customize --mkdir ${build_info_file_location} --copy-in ${install_dir}build-info:${build_info_file_location} -a ${image_name}
 qm destroy ${build_vm_id}
-qm create ${build_vm_id} --memory ${vm_mem} --cores ${vm_cores} --net0 virtio,bridge=vmbr0 --name $(template-name)
+qm create ${build_vm_id} --memory ${vm_mem} --cores ${vm_cores} --net0 virtio,bridge=vmbr0 --name ${template_name}
 qm importdisk ${build_vm_id} ${image_name} ${storage_location}
 qm set ${build_vm_id} --scsihw ${scsihw} --scsi0 ${storage_location}:vm-${build_vm_id}-disk-0
 qm set ${build_vm_id} --ide0 ${storage_location}:cloudinit
-qm set ${build_vm_id} --nameserver ${nameserver} --ostype l26 --searchdomain ${searchdomain} --sshkeys ${keyfile} --ciuser $(cloud-init-user)
+qm set ${build_vm_id} --nameserver ${nameserver} --ostype l26 --searchdomain ${searchdomain} --sshkeys ${keyfile} --ciuser ${cloud_init_user}
 qm set ${build_vm_id} --boot c --bootdisk scsi0
 #qm set ${build_vm_id} --serial0 socket --vga serial0
 qm set ${build_vm_id} --agent enabled=1
